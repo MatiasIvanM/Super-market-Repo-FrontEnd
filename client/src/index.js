849f5d86a9fc61693483bfca8850cb5b1332c40d
Santiago Martin Solavaggione <108992013+Santjagor@users.noreply.github.com>
2023-08-30 14:14:42 -0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,27 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-<<<<<<< .merge_file_9Dcth5
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
-=======
 import { Auth0Provider } from '@auth0/auth0-react'
+import {BrowserRouter} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
->>>>>>> .merge_file_Qab7M3
 
 ReactDOM.render(
   <React.StrictMode>
-<<<<<<< .merge_file_9Dcth5
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>,
-  </React.StrictMode>,
-  document.getElementById('root')
-=======
     <Auth0Provider
       domain={domain}
       clientId={clientId}
@@ -30,10 +19,12 @@ ReactDOM.render(
         redirect_uri: window.location.origin
       }}
     >
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Auth0Provider>
-  </React.StrictMode>
->>>>>>> .merge_file_Qab7M3
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
