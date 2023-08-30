0613b21c3494a12d068f1691e672369fa374f0ef
ALoyoVasquez <anitamlv@gmail.com>
2023-08-30 14:13:23 -0400

    Rama AMLV

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+// import CardFooter from 'react-bootstrap/CardFooter'
+
+//Footer con el about, preguntas frecuentes, como comprar?  contactanos (mail),
+const Footer = () => {
+  return (
+    <div color="red">
+      <div>CONOCENOS</div>
+      <div>PREGUNTAS FRECUENTES</div>
+      <div>¿CÓMO COMPRAR?</div>
+      <div>CONTÁCTANOS</div>
+      <div>
+        © 2023 Copyright: PF-SuperMarket-Shop
+      </div>
+    </div>
+  )
+}
+
+export default Footer
+
