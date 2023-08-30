9c403c87ed98827f2e42e4bd5bf525dc1d049d37
Santiago Martin Solavaggione <108992013+Santjagor@users.noreply.github.com>
2023-08-30 14:20:16 -0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Auth0Provider } from '@auth0/auth0-react'
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
 
