95db5908d1dbd5056b1f55881cc0ce69c7f63c9a
Santiago Martin Solavaggione <santirigore@gmail.com>
2023-08-29 22:05:58 -0300

    prueba

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from "react-router-dom";
 import { Auth0Provider } from '@auth0/auth0-react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
@@ -9,8 +10,7 @@ import reportWebVitals from './reportWebVitals';
 const domain = process.env.REACT_APP_AUTH0_DOMAIN
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Auth0Provider
       domain={domain}
@@ -19,11 +19,13 @@ root.render(
         redirect_uri: window.location.origin
       }}
     >
-      <App />
-    </Auth0Provider>
-  </React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Auth0Provider>,
+  </React.StrictMode >,
+  document.getElementById('root')
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
