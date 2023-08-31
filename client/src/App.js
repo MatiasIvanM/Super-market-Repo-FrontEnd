515533834a2e0c57c70b32f5a17e2580478fd2ec
MatiasIvanM <matiasivan.sd@gmail.com>
2023-08-31 20:29:01 -0300

    Fix Archivos y recuperacion landing

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer/Footer';
+import Landing from './View/Landing/Landing';
 
 function App() {
   return (
@@ -12,5 +13,5 @@ function App() {
     </div>
   );
 }
-
+//Se esta renderizando doble el footer y el navbar, por el render desde landing
 export default App;
