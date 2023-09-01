de80ea8d6541c44cd9244e265252ec37d801c00a
Santiago Martin Solavaggione <santirigore@gmail.com>
2023-08-31 21:11:14 -0300

    App

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
+import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
+import Footer from './components/Footer/Footer';
+import Landing from './View/Landing/Landing';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Landing/>
+      <Footer/>
     </div>
   );
 }
