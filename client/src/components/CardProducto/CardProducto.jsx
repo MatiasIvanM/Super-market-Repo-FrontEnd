b74e8922bcac047d1471bdbf6eccca0733895c73
ALoyoVasquez <anitamlv@gmail.com>
2023-09-04 20:39:22 -0400

    card

--- client/src/components/CardProducto/CardProducto.jsx
+++ client/src/components/CardProducto/CardProducto.jsx
@@ -4,6 +4,17 @@ import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import style from "./CardProduct.module.css";
 import { Link } from "react-router-dom";
 
+// "Product",
+//     { id:            { type: DataTypes.UUID, primaryKey: true },
+//       name:          { type: DataTypes.STRING, allowNull: false },
+//       price:         { type: DataTypes.FLOAT, allowNull: false },
+//       description:   { type: DataTypes.TEXT, allowNull: false },
+//       image:         { type: DataTypes.STRING, allowNull: false },
+//       stock:         { type: DataTypes.INTEGER, defaultValue: 1 },
+//       brand:         { type: DataTypes.STRING, allowNull: false, },
+//       expirationdate: { type: DataTypes.DATE, allowNull: false, },
+//       categories:    { type: DataTypes.STRING, allowNull: false },
+//     },
 
 function CardProduct(props) {
   let {
@@ -22,29 +33,49 @@ function CardProduct(props) {
 
   return (
     <Card style={{ width: "16rem" }} bg="white">
-      <Link to={`/detail/${id}`}  className={style.link}>
-        <Card.Img variant="top" src={image} style={{ width: "70%", alignSelf: "center" }} />
+      <Link to={`/product/${id}`}  className={style.link}>
+        <Card.Img
+          variant="top"
+          src={image}
+          style={{ width: "70%", alignSelf: "center" }}
+        />
         <Card.Subtitle className={style.rating}>★{rating}</Card.Subtitle>
         <Card.Body style={{ width: "98%" }}>
           <Card.Title style={{ fontSize: "1.12rem" }}>{name}</Card.Title>
-          <Card.Text style={{ fontSize: "0.7rem", color: "gray", textAlign: "left" }}>
-            {description}
+          <Card.Text
+            style={{ fontSize: "0.7rem", color: "gray", textAlign: "left" }}
+          >
+            {description}{" "}
           </Card.Text>
           <div className={style.containerPrice}>
-            <Card.Title style={{ fontSize: "1.3rem", color: "blue", textAlign: "left" }}>
+            <Card.Title
+              style={{ fontSize: "1.3rem", color: "blue", textAlign: "left" }}
+            >
               ${price}
             </Card.Title>
-            {Existencias 
-            ? (<Card.Text style={{ fontSize: "0.7rem", fontWeight: "bold", color: "green", }} > Disponible </Card.Text>) 
-            : (<Card.Text style={{ fontSize: "0.7rem", fontWeight: "bold", color: "red" }}> Agotado </Card.Text>)
-            }
+            {Existencias ? (
+              <Card.Text
+                style={{
+                  fontSize: "0.7rem",
+                  fontWeight: "bold",
+                  color: "green",
+                }}
+              >
+                Disponible{" "}
+              </Card.Text>
+            ) : (
+              <Card.Text
+                style={{ fontSize: "0.7rem", fontWeight: "bold", color: "red" }}
+              >
+                Agotado{" "}
+              </Card.Text>
+            )}
           </div>
-        </Card.Body>
-      </Link>
-          <Button variant="primary" style={{ width: "98%" }} 
-                  onClick={()=>{alert(`Agregar producto: ${name}`)}}>
+          <Button variant="primary" style={{ width: "98%" }}>
             Agregar
           </Button>
+        </Card.Body>
+      </Link>
     </Card>
     // </>
   );
