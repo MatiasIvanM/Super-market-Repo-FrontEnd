e01b353a28760dc847bcbf0190edeab81669946f
MatiasIvanM <matiasivan.sd@gmail.com>
2023-09-07 20:08:34 -0300

    form

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,9 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Landing from './View/Landing/Landing';
 import Home from './View/Home/Home.jsx'
 import ProductsDetail from '../src/View/Detail/Detail'
-import FormProduct from "./View/FormProduct/FormProduct"
+import FormProduct from "./View/FormProduct/formProduct"
+import MercadoPagoCheckout from "./components/MerPago/MercadoPagoCheckout"
+
 
 
 
@@ -16,6 +18,7 @@ function App() {
       <Switch>
         <Route path="/" exact component={Landing} />
         <Route path="/home" component={Home} />
+        <Route path="/mercadopago" component={MercadoPagoCheckout}/>
         <Route path="/product/:id" component={ProductsDetail} />
         <Route path="/form" component={FormProduct} />
       </Switch>
