4b34d059ab7ce3265ea99bacdbcfda8b7b96688d
Nicolas Castellano <felipenicolas115@gmail.com>
2023-09-10 10:58:43 -0300

    avances de SC

--- client/src/View/Detail/Detail.jsx
+++ client/src/View/Detail/Detail.jsx
@@ -6,6 +6,7 @@ import { Button, Modal, Card, Alert, Spinner } from 'react-bootstrap';
 import Image from 'react-bootstrap/Image';
 import style from './Detail.module.css'
 import { AiOutlineStar } from 'react-icons/ai'
+import {addProductSC} from "../../redux/Actions/actionsSC"
 
 function ProductsDetail(props) {
     
@@ -30,6 +31,9 @@ function ProductsDetail(props) {
 
     function handleAddToCart() {
       setShowMessage(true);
+      let productSC= {productDetails,quantity}
+      console.log(productSC)
+      dispatch(addProductSC(productSC))
         setTimeout(() => {
             setShowMessage(false);
         }, 2000);
--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,6 @@ import { BrowserRouter } from 'react-router-dom';
 import store from './redux/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import  App  from './App';
-import { CartProvider } from 'react-use-cart';
 // import reportWebVitals from './reportWebVitals';
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
@@ -17,9 +16,7 @@ const app = (
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-      <CartProvider> 
           <App />
-        </CartProvider>
       </BrowserRouter>
     </Provider>
     <Auth0Provider
