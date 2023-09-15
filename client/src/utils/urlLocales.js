c40f1b6763430600cddd77d922073587a6c89e0b
Santiago Martin Solavaggione <santirigore@gmail.com>
2023-09-15 18:38:41 -0300

    f

--- client/src/utils/urlLocales.js
+++ client/src/utils/urlLocales.js
@@ -6,9 +6,7 @@
 //const CATEGORY='http://localhost:3001/category'
 //const server ='http://localhost:3001/'
 // backend
-// const server = 'https://super-market-shop-preview.up.railway.app';
-const server = 'http://localhost:3001';
-
+const server = 'https://super-market-shop-preview.up.railway.app';
 
  const PRODUCT=`${server}/product/`;
  const CUSTOMER=`${server}/customer/`;
