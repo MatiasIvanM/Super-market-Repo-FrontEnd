8c500ad50858dbf4ef552718bcc0320d323812a7
glossada <gabriellossada@gmail.com>
2023-09-18 10:45:49 -0300

    Revert "mejora scroll infinito"
    This reverts commit a2461cefa4c3c2627474d06a44d5bc7fd0673240.

--- client/src/View/Home/Home.jsx
+++ client/src/View/Home/Home.jsx
@@ -10,7 +10,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { filterByCategory, orderPrecio, getProductsByName, rangoPrecios, getProducts } from '../../redux/Actions/actionsProducts';
 import { selectCategory } from '../../redux/Actions/actionsCategory';
 import InfiniteScroll from 'react-infinite-scroll-component';
-import LoadGif from '../../components/Loading/shopping-cart-shopping.gif'
 
 export default function Home() {
     const dispatch = useDispatch();
@@ -23,7 +22,6 @@ export default function Home() {
     const [currentPage, setCurrentPage] = useState(0);
     const [items, setItems] = useState([]);
     const [customer, setCustomer] = useState({});
-    const [showLoader, setShowLoader] = useState(false);
     let customerById = useSelector((state)=>state.customerId)
     let categories=useSelector((state)=>state.category)
     
@@ -32,32 +30,24 @@ export default function Home() {
 
     }, [customerById]);
     const nextHandler = () => {
-        setShowLoader(true);
+        const totalElements = productsMod.length;
+        const nextPage = currentPage + 1;
+        const firstIndex = nextPage * ITEMS_PER_PAGE;
 
-        setTimeout(() => {
-            const totalElements = productsMod.length;
-            const nextPage = currentPage + 1;
-            const firstIndex = nextPage * ITEMS_PER_PAGE;
-    
-            if (firstIndex >= totalElements) {
-                setShowLoader(false);
-                return;
-            }
-    
-            setItems(items.concat([...productsMod].splice(firstIndex, ITEMS_PER_PAGE)));
-            setCurrentPage(nextPage);
-            setShowLoader(false);
-        }, 2000);
+        if (firstIndex >= totalElements) return;
+
+        setItems(items.concat([...productsMod].splice(firstIndex, ITEMS_PER_PAGE)));
+        setCurrentPage(nextPage);
     };
 
-    // const prevHandler = () => {
-    //     const prevPage = currentPage - 1;
-    //     if (prevPage < 0) return;
-    //     const firstIndex = prevPage * ITEMS_PER_PAGE;
+    const prevHandler = () => {
+        const prevPage = currentPage - 1;
+        if (prevPage < 0) return;
+        const firstIndex = prevPage * ITEMS_PER_PAGE;
 
-    //     setItems([...productsMod].splice(firstIndex, ITEMS_PER_PAGE));
-    //     setCurrentPage(prevPage);
-    // };
+        setItems([...productsMod].splice(firstIndex, ITEMS_PER_PAGE));
+        setCurrentPage(prevPage);
+    };
 
 
     const searchByName = (name) => {
@@ -190,7 +180,7 @@ export default function Home() {
       dataLength={items.length} // Tamaño actual de la lista de productos
       next={nextHandler} // Función a ejecutar cuando se necesita cargar más productos
       hasMore={true} // Indica si hay más productos por cargar (puede cambiarlo según tu lógica)
-      loader={showLoader ? <img className={styles.load_gif} src={LoadGif} alt="Cargando..." />: null}
+      loader={<h4>Cargando...</h4>}
       className={styles.card_container}
     >
                      {items.map(p => (
