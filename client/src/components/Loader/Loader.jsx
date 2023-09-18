67ee06076719352ca35f0583e3d5fdbd3122fa08
Santiago Martin Solavaggione <santirigore@gmail.com>
2023-09-18 13:27:31 -0300

    k

--- client/src/components/Loader/Loader.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './Loader.module.css'
-
-export default function Loader() {
-    return (
-        <div className={styles.container}>
-            <span className={styles.loader}></span>
-        </div>
-    )
-}
