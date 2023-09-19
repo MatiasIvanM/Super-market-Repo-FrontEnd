73a3c5588678c949b1bd1f3dcc465ab3cba289da
Santiago Martin Solavaggione <santirigore@gmail.com>
2023-09-19 17:24:01 -0300

    delete newToken function

--- client/src/utils/newToken.js
+++
@@ -1,8 +0,0 @@
-import { useAuth0 } from "@auth0/auth0-react"
-
-export default function newToken() {
-    const { isAuthenticated, getIdTokenClaims } = useAuth0()
-    if (isAuthenticated) {
-        
-    }
-}
