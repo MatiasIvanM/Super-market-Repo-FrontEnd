e45dfdc48dfd289f734d97e7d65e35a8094f6c64
MatiasIvanM <105732530+MatiasIvanM@users.noreply.github.com>
2023-09-26 00:14:34 -0300

    Update urlLocales.js

--- client/src/utils/urlLocales.js
+++ client/src/utils/urlLocales.js
@@ -1,25 +1,25 @@
 // LOCAL
 
-const PRODUCT='http://localhost:3001/product/';
-const CUSTOMER='http://localhost:3001/customer/'
-const ORDER_DETAIL='http://localhost:3001/orderDetail/'
-const ORDERS='http://localhost:3001/orders/'
-const SHOPPINGCART="http://localhost:3001/shoppingCart/"
-const CATEGORY='http://localhost:3001/category'
-const COMMENT='http://localhost:3001/comment'
-const server ='http://localhost:3001/'
+//const PRODUCT='http://localhost:3001/product/';
+//const CUSTOMER='http://localhost:3001/customer/'
+//const ORDER_DETAIL='http://localhost:3001/orderDetail/'
+//const ORDERS='http://localhost:3001/orders/'
+//const SHOPPINGCART="http://localhost:3001/shoppingCart/"
+//const CATEGORY='http://localhost:3001/category'
+//const COMMENT='http://localhost:3001/comment'
+//const server ='http://localhost:3001/'
 
 
 
 // backend
 
-//  const server = 'https://super-market-shop-preview.up.railway.app';
-//   const PRODUCT=`${server}/product/`;
-//   const CUSTOMER=`${server}/customer/`;
-//   const ORDER_DETAIL=`${server}/orderDetail/`;
-//   const ORDERS=`${server}/orders/`;
-//   const SHOPPINGCART=`${server}/shoppingCart/`;
-//   const CATEGORY=`${server}/category/`;
-//   const COMMENT=`${server}/comment/`
+  const server = 'https://super-market-shop-preview.up.railway.app';
+  const PRODUCT=`${server}/product/`;
+  const CUSTOMER=`${server}/customer/`;
+  const ORDER_DETAIL=`${server}/orderDetail/`;
+  const ORDERS=`${server}/orders/`;
+  const SHOPPINGCART=`${server}/shoppingCart/`;
+  const CATEGORY=`${server}/category/`;
+  const COMMENT=`${server}/comment/`
 
 export {PRODUCT,CUSTOMER,ORDER_DETAIL,ORDERS,SHOPPINGCART,CATEGORY, COMMENT, server};
