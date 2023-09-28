863b949ae9ed2fbe0455e216a1edb3cd50c8cf0f
Bautizitelli <bautista.zitelli11@gmail.com>
2023-09-28 17:21:13 -0300

    Historial de compras y comentarios sobre los productos

--- client/src/View/Profile/Profile.jsx
+++ client/src/View/Profile/Profile.jsx
@@ -1,19 +1,18 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Modal from 'react-bootstrap/Modal';
+import {Button, Card, Form, InputGroup, Modal, Accordion, DropdownButton, Row, Col} from 'react-bootstrap'
 import { useAuth0 } from "@auth0/auth0-react"
-import { Link } from 'react-router-dom';
-import { useHistory } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import Overlay from '../../components/Overlay/Overlay';
 import { getCustomerByEmail, getCustomerById, modCustomer } from '../../redux/Actions/actionsCustomers';
 import * as validate from '../Register/validations'
 import { PiWarning } from 'react-icons/pi'
+import { VscTasklist } from 'react-icons/vsc'
 import styles from './Profile.module.css'
 import {clearSC,clearCustomerId} from "../../redux/Actions/actionsSC"
+import { getBuy } from "../../redux/Actions/actionsBuy"
+import  Comments  from "../../components/Comments/AddComments"
+
 import { Footer } from '../../components/Footer/Footer';
 
 export default function Profile() {
@@ -22,6 +21,13 @@ export default function Profile() {
     const history = useHistory()
     const [edit, setEdit] = useState(true)
     const [customer, setCustomer] = useState()
+    const [customerData, setCustomerData] = useState()
+    const [profilePurchase, setProfilePurchase] = useState([]);
+    const [buyShow, setBuyShow] = useState(false);
+
+    const handleClose = () => setBuyShow(false);
+
+
     const [modal, setModal] = useState({
         show: false,
         header: "",
@@ -34,23 +40,11 @@ export default function Profile() {
         phone: "",
     })
 
-    // async function getCustomer() {
-    //     const lsCustomer = JSON.parse(localStorage.getItem('customer'))
-    //     const user = await dispatch(getCustomerByEmail(lsCustomer.email))
-    //     localStorage.setItem('customer', JSON.stringify({
-    //         id: user.payload[0].id,
-    //         name: user.payload[0].name,
-    //         email: user.payload[0].email,
-    //         role: user.payload[0].role,
-    //     }))
-    //     setCustomer({ ...user.payload[0] })
-    // }
     async function getCustomer() {
         const lsCustomer = JSON.parse(localStorage.getItem('customer'));
-        
+
         if (lsCustomer) {
           const user = await dispatch(getCustomerByEmail(lsCustomer.email));
-      
           if (user.payload && user.payload.length > 0) {
             localStorage.setItem('customer', JSON.stringify({
               id: user.payload[0].id,
@@ -59,6 +53,8 @@ export default function Profile() {
               role: user.payload[0].role,
             }));
             setCustomer({ ...user.payload[0] });
+            setCustomerData(lsCustomer)
+
           } else {
             console.error('No se encontró un usuario con el correo electrónico proporcionado');
           }
@@ -66,7 +62,66 @@ export default function Profile() {
           console.error('No hay un cliente en el almacenamiento local');
         }
       }
-      
+
+    const purchaseHistory = async () => {
+        setBuyShow(true);
+        const res = await dispatch(getBuy());
+        const buy = res.payload;
+        const profileId = customerData.id;
+
+        const filteredPurchases = buy.filter((buy) => buy.CustomerId === profileId);
+
+        if (filteredPurchases.length > 0) {
+          console.log('Compras del perfil:', filteredPurchases);
+          setProfilePurchase(filteredPurchases);
+        } else {
+          console.log('No se encontraron compras que coincidan con el perfil.');
+        }
+      }
+      function formatDate(dateTimeString) {
+        const date = new Date(dateTimeString);
+        
+        const daysOfWeek = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+        const dayOfWeek = daysOfWeek[date.getDay()];
+        
+        const day = date.getDate().toString().padStart(2, "0");
+        const month = (date.getMonth() + 1).toString().padStart(2, "0");
+        const year = date.getFullYear();
+        
+        const hours = date.getHours().toString().padStart(2, "0");
+        const minutes = date.getMinutes().toString().padStart(2, "0");
+        
+        return `${dayOfWeek} ${day}/${month}/${year} a las ${hours}:${minutes}`;
+    }
+
+    function getPaymentMethodLabel(paymentMethod) {
+        switch (paymentMethod) {
+            case "master":
+                return "Tarjeta Master Card";
+            case "visa":
+                return "Tarjeta Visa";
+            case "amex":
+                return "Tarjeta American Express";
+            case "account_money":
+                return "Dinero en cuenta";
+            default:
+                return paymentMethod;
+        }
+    }
+    
+    function getStatusLabel(status) {
+        switch (status) {
+            case "approved":
+                return "Aprobado";
+            case "rejected":
+                return "Rechazado";
+            case "pending":
+                return "Pendiente";
+            default:
+                return status;
+        }
+    }
+
 
     function handleLogout() {
         localStorage.clear()
@@ -139,6 +194,7 @@ export default function Profile() {
         }
     }
 
+
     function handleModalButton() {
         setModal({ ...modal, show: false })
     }
@@ -168,6 +224,7 @@ export default function Profile() {
                 <Card.Body>
                     <Card.Title>
                         <div className={styles.name}>{customer?.name}</div>
+                        <Button onClick={purchaseHistory} variant="outline-dark"><VscTasklist/> Mis Compras</Button>
                     </Card.Title>
                     <Card.Subtitle>
                         <div>{customer?.email}</div>
@@ -206,6 +263,74 @@ export default function Profile() {
             </Card>
             <Button as={Link} to='/home' variant='secondary' size='sm'>Volver al inicio</Button>
             <br />
+
+            <Modal
+                size="lg"
+                show={buyShow}
+                onHide={() => setBuyShow(false)}
+                aria-labelledby="example-modal-sizes-title-lg"
+            >
+                <Modal.Header closeButton>
+                <Modal.Title id="example-modal-sizes-title-lg">
+                    Tus compras
+                </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                {profilePurchase.length > 0 ? (
+                     <Accordion defaultActiveKey="0" flush>
+                    {profilePurchase.map((purchase) => (
+                          <Accordion.Item >
+                          <Accordion.Header key={purchase.id}> Compra realizada el {formatDate(purchase.fechapago)}</Accordion.Header>
+                          <Accordion.Body>
+                        {purchase.cart.map((product) => (
+                            <div key={product.id} className={styles.productItem}>
+                            <div className={styles.productContainer}>
+                              <img src={product.picture_url} alt={product.title} className={styles.productImage} />
+                              <div className={styles.productInfo}>
+                                <h5 className={styles.title}>{product.title}</h5>
+                                <p className={styles.productDescription}>{product.description}</p>
+                                <p className={styles.productDescription}>{product.quantity} Unidades</p>
+                                <p className={styles.productDescription}>Precio unitario: ${product.unit_price}</p>
+                              </div>
+                            </div>
+
+                              <div className={styles.commentContainer}>
+                                <DropdownButton id="dropdown-item-button" title="Opina sobre este producto !">
+                                  <div className={styles.comments}>
+                                    <Comments
+                                        productId={product.id}
+                                        customerId={customerData.id}
+                                    />
+                                  </div>
+                                </DropdownButton>
+                              </div>
+
+                          </div>
+
+                        ))}
+                            <Row className={styles.customRow}>
+                            <Col>
+                                Método de pago :
+                            </Col>
+                            <Col className={styles.customCol}>
+                                {getPaymentMethodLabel(purchase.card.payment_method.id)}
+                            </Col>
+                            <Col> Total pagado: ${purchase.total}</Col>
+                            <Col>
+                                Estado: {getStatusLabel(purchase.estado)}
+                            </Col>
+                          </Row>
+                        </Accordion.Body>
+                        
+                         </Accordion.Item>
+                    ))}
+                   </Accordion>
+                ):(
+                    <p>Todavia no hay compras asociadas a esta cuenta.</p>
+                )}
+                </Modal.Body>
+            </Modal>
+
             <Modal show={modal.show}>
                 <Modal.Body className={styles.modal}>
                     <Modal.Header>
--- client/src/components/Comments/AddComments.jsx
+++ client/src/components/Comments/AddComments.jsx
@@ -1,19 +1,21 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addComment } from '../../redux/Actions/actionsComments';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Alert, Col, Row } from 'react-bootstrap';
 import RatingSelector from './RatingSelector';
+import style from './Comment.module.css'
 
 function Comments(props) {
-    const { productId } = props;
+    const { productId,customerId } = props;
 
-    const customer = useSelector((state) => state.customerId);
-    const customerId = customer.id;
+    // const customer = useSelector((state) => state.customerId);
+    // const customerId = customer.id;
 
     const dispatch = useDispatch();
     const comments = useSelector((state) => state.comments);
     const [comment, setComment] = useState('');
     const [calification, setCalification] = useState(0);
+    const [showSuccessAlert, setShowSuccessAlert] = useState(false);
 
     //comment form
     const handleCommentChange = (e) => {
@@ -36,6 +38,11 @@ function Comments(props) {
         dispatch(addComment(newComment));
 
         setComment('');
+        setShowSuccessAlert(true);
+
+        setTimeout(() => {
+            setShowSuccessAlert(false);
+        }, 3000);
     };
 
     //rating selector
@@ -44,10 +51,11 @@ function Comments(props) {
     };
 
     return (
-        <div>
-            <h2>Deja un comentario</h2>
-            <RatingSelector onChange={setCalification} />
-
+        <div className={style.commentContainer}>
+            <h3>Deja tu comentario</h3>
+            <div className={style.ratingSelector}>
+                <RatingSelector onChange={setCalification}/>
+            </div>
             <Form.Group controlId="commentForm">
                 <Form.Control
                     as="textarea"
@@ -56,9 +64,24 @@ function Comments(props) {
                     value={comment}
                     onChange={handleCommentChange}
                 />
-                <Button variant="primary" onClick={handleAddComment}>
-                    Agregar Comentario
-                </Button>
+            <div className={style.commentFooter}>
+                <Row>
+                    <Col>
+                    <Button variant="primary" onClick={handleAddComment} className={style.customButton}>
+                        Enviar comentario
+                    </Button>
+                    </Col>
+                    <Col>
+                    {showSuccessAlert && (
+                            <Alert variant="success" className={style.customAlert}>
+                                <div className={style.alertContent}>
+                                    Comentario enviado !
+                                </div>
+                            </Alert>
+                        )}
+                    </Col>
+                </Row>
+            </div>
             </Form.Group>
         </div>
     );
